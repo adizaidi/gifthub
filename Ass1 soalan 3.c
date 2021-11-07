e3b83e1d7759df04176ff9eb0ddebcc0b7a0da0c
adizaidi <93848284+adizaidi@users.noreply.github.com>
2021-11-07 09:26:44 +0800

    Add files via upload

---
+++ Ass1 soalan 3.c
@@ -0,0 +1,22 @@
+//documentation section
+/*Syazwan Yusof,4/11/2021,assignment 1 question 3*/
+
+//pre-processor section
+#include<stdio.h>
+
+//global variable section
+
+//main function section
+int main(){
+    int integerValue =100;
+    printf("this is a integer value %d ",integerValue);
+
+    float decimalValue=0.125;
+    printf("this is decimal value %.3f ",decimalValue);
+
+    char Value='i';
+    printf("this is charvalue %c ",Value);
+}
+
+
+
